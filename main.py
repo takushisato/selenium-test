78737c7b0feb38dc23ed3cb3af3e56fb77b13c3c
taku820801 <takuchanpower@gmail.com>
2024-12-31 15:20:04 +0900

    googleの検索

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service = Service(executable_path='chromedriver')
@@ -10,6 +14,12 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Chrome(service=service, options=options)
 
+# Googleでseleniumを検索
 driver.get('https://www.google.com')
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'gLFyf'))
+)
+input_element = driver.find_element(By.CLASS_NAME, 'gLFyf')
+input_element.send_keys('selenium' + Keys.ENTER)
 time.sleep(10)
 driver.quit()
