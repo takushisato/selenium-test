4bed2c86aac29a6042a0a7202e113d1a1f2925c1
taku820801 <takuchanpower@gmail.com>
2024-12-31 16:59:47 +0900

    ログインからDM先ユーザー画面を開くところまでOK

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ from dotenv import load_dotenv
 
 # 環境変数からXの情報を読み込み
 load_dotenv()
-account_name = os.getenv('ACCOUNT_NAME')
+from_account = os.getenv('FROM_ACCOUNT')
 password = os.getenv('PASSWORD')
+to_account = os.getenv('TO_ACCOUNT')
 
 # ChromeDriverの設定
 service = Service(executable_path='chromedriver')
@@ -20,16 +21,17 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=service, options=options)
 
-# Xのログイン画面を開く
-driver.get('https://x.com/i/flow/login')
 
 try:
+    # DM送信元アカウントのログイン画面を開く
+    driver.get('https://x.com/i/flow/login')
+
     # アカウント入力画面まで待機。開いたらアカウント名の入力
-    input_account_name = WebDriverWait(driver, 20).until(
+    from_account_name = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.XPATH, '//input[@name="text"]'))
     )
-    input_account_name.send_keys(account_name)
-    input_account_name.send_keys(Keys.RETURN)
+    from_account_name.send_keys(from_account)
+    from_account_name.send_keys(Keys.RETURN)
 
     # パスワード画面まで待機。開いたらパスワードの入力
     input_password = WebDriverWait(driver, 20).until(
@@ -37,6 +39,28 @@ try:
     )
     input_password.send_keys(password)
     input_password.send_keys(Keys.RETURN)
+
+    # ログインが完了するまで10秒待機
+    time.sleep(10)
+
+    # DM送信先のユーザーのHOME画面を開く
+    to_account_home = 'https://x.com/' + to_account
+    driver.get(to_account_home)
+
+
+    # DMボタンを探してクリック
+    # dm_button = WebDriverWait(driver, 20).until(
+    #     EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Message"]'))  # DMボタンを特定
+    # )
+    # dm_button.click()
+    #
+    # # メッセージを送信
+    # message_box = WebDriverWait(driver, 20).until(
+    #     EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Message body"]'))  # メッセージ入力欄を特定
+    # )
+    # message = "こんにちは！自動メッセージです。"
+    # message_box.send_keys(message)
+    # message_box.send_keys(Keys.RETURN)
 
 finally:
     # 10秒後にブラウザを閉じる
