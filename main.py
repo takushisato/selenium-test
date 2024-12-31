4a1eeec109cb226d35736dce464c6b5b6682cd8b
taku820801 <takuchanpower@gmail.com>
2024-12-31 14:57:45 +0900

    googleを開くコードを追加

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# これはサンプルの Python スクリプトです。
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
-# ⌃R を押して実行するか、ご自身のコードに置き換えてください。
-# ⇧ を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
+service = Service(executable_path='chromedriver')
+driver = webdriver.Chrome(service=service)
 
-
-def print_hi(name):
-    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # ⌘F8を押すとブレークポイントを切り替えます。
-
-
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
+driver.get('https://www.google.com')
+time.sleep(10)
+driver.quit()
